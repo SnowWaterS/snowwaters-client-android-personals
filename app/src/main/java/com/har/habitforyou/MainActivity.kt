d4481b90b111d5ddbfdf32e1d133b93d59a1c59d
SnowWaters <harharr@gmail.com>
2020-10-26 10:56:52 +0900

    Adapt DataBinding

--- app/src/main/java/com/har/habitforyou/MainActivity.kt
+++ app/src/main/java/com/har/habitforyou/MainActivity.kt
@@ -6,22 +6,39 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import com.har.habitforyou.ui.main.SectionsPagerAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.har.habitforyou.databinding.ActivityMainBinding
+import com.har.habitforyou.ui.main.MainFragment
+import com.har.habitforyou.ui.sections.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = _binding ?: throw NullPointerException("Manager Activity Binding is null")
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
 
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.viewModel = viewModel
+
+//        val sectionsPagerAdapter =
+//            SectionsPagerAdapter(
+//                this,
+//                supportFragmentManager
+//            )
+//        val viewPager: ViewPager = findViewById(R.id.view_pager)
+//        viewPager.adapter = sectionsPagerAdapter
+//        val tabs: TabLayout = findViewById(R.id.tabs)
+//        tabs.setupWithViewPager(viewPager)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
---
+++ app/src/main/java/com/har/habitforyou/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.har.habitforyou
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+}
