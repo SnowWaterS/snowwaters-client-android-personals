5853b3571dd0aa28fad39daa97bf835cbeaf270f
SnowWaters <harharr@gmail.com>
2020-10-27 01:41:35 +0900

    Update MainActivity (Add main drawer and remove Toolbar)

--- app/src/main/java/com/har/habitforyou/MainActivity.kt
+++ app/src/main/java/com/har/habitforyou/MainActivity.kt
@@ -1,24 +1,36 @@
 package com.har.habitforyou
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.MenuItem
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
+import com.google.android.material.navigation.NavigationView
 import com.har.habitforyou.databinding.ActivityMainBinding
 import com.har.habitforyou.ui.main.MainFragment
 import com.har.habitforyou.ui.sections.SectionsPagerAdapter
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private var _binding: ActivityMainBinding? = null
     private val binding: ActivityMainBinding
         get() = _binding ?: throw NullPointerException("Manager Activity Binding is null")
 
     private lateinit var viewModel: MainViewModel
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,20 +40,32 @@ class MainActivity : AppCompatActivity() {
 
         binding.viewModel = viewModel
 
-//        val sectionsPagerAdapter =
-//            SectionsPagerAdapter(
-//                this,
-//                supportFragmentManager
-//            )
-//        val viewPager: ViewPager = findViewById(R.id.view_pager)
-//        viewPager.adapter = sectionsPagerAdapter
-//        val tabs: TabLayout = findViewById(R.id.tabs)
-//        tabs.setupWithViewPager(viewPager)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        navView.setupWithNavController(navController)
+        navView.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        Log.d("Main", "onNavigationItemSelected: ${item.title}")
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
 }
