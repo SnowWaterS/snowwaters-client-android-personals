ecba63e5a3745589cb0677a776bffab4afe60222
SnowWaters <harharr@gmail.com>
2020-10-29 00:39:48 +0900

    [Printer] Add TabLayout and ViewPager2

--- app/src/main/java/com/har/habitforyou/ui/printer/PrinterFragment.kt
+++ app/src/main/java/com/har/habitforyou/ui/printer/PrinterFragment.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.har.habitforyou.R
+import com.har.habitforyou.databinding.FragmentPrinterBinding
 
 class PrinterFragment : Fragment() {
 
@@ -14,17 +19,32 @@ class PrinterFragment : Fragment() {
         fun newInstance() = PrinterFragment()
     }
 
-    private lateinit var fragmentViewModel: PrinterFragmentViewModel
+    private var _binding: FragmentPrinterBinding? = null
+    private val binding get() = _binding ?: throw NullPointerException("FragmentPrinterBinding is Null")
+    private lateinit var viewModel: PrinterFragmentViewModel
+    private lateinit var viewPager: ViewPager2
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_printer, container, false)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_printer, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        fragmentViewModel = ViewModelProvider(this).get(PrinterFragmentViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(PrinterFragmentViewModel::class.java)
+        binding.viewModel = viewModel
+
+        val printerPagerAdapter = PrinterPagerAdapter(this)
+        binding.vpPrinter.adapter = printerPagerAdapter
+        binding.vpPrinter.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        TabLayoutMediator(binding.tlPrinter, binding.vpPrinter,
+            TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+                tab.text = getString(printerPagerAdapter.getTitleResId(position))
+                tab.setIcon(printerPagerAdapter.getIconResId(position))
+            }).attach()
     }
 }
---
+++ app/src/main/java/com/har/habitforyou/ui/printer/PrinterPagerAdapter.kt
@@ -0,0 +1,68 @@
+package com.har.habitforyou.ui.printer
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.har.habitforyou.R
+import com.har.habitforyou.ui.printer.tab.extension.PrinterExtensionFragment
+import com.har.habitforyou.ui.printer.tab.image.PrinterImageFragment
+import com.har.habitforyou.ui.printer.tab.settings.PrinterSettingsFragment
+import com.har.habitforyou.ui.printer.tab.text.PrinterTextFragment
+
+class PrinterPagerAdapter(fragment: Fragment): FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int {
+        return 4
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                PrinterTextFragment()
+            }
+            1 -> {
+                PrinterImageFragment()
+            }
+            2 -> {
+                PrinterExtensionFragment()
+            }
+            else -> {
+                PrinterSettingsFragment()
+            }
+        }
+    }
+
+    fun getTitleResId(position: Int): Int {
+        return when (position) {
+            0 -> {
+                R.string.printer_title_text
+            }
+            1 -> {
+                R.string.printer_title_image
+            }
+            2 -> {
+                R.string.printer_title_extension
+            }
+            else -> {
+                R.string.printer_title_settings
+            }
+        }
+    }
+
+    fun getIconResId(position: Int): Int {
+        return when (position) {
+            0 -> {
+                R.drawable.ic_priner_text
+            }
+            1 -> {
+                R.drawable.ic_printer_image
+            }
+            2 -> {
+                R.drawable.ic_printer_extension
+            }
+            else -> {
+                R.drawable.ic_printer_settings
+            }
+        }
+    }
+
+}
