03402db9d06dff2e03ab244d221b4d1902516c87
SnowWaters <harharr@gmail.com>
2020-11-03 03:06:07 +0900

    [Main] SplashFragment -> SplashActivity & Adopt Coroutines

---
+++ app/src/main/java/com/har/habitforyou/MainSplashActivity.kt
@@ -0,0 +1,114 @@
+package com.har.habitforyou
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+import kotlin.system.exitProcess
+
+
+class MainSplashActivity : AppCompatActivity(), CoroutineScope {
+
+    private val job = SupervisorJob()
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            val controller = window.insetsController
+            controller?.apply {
+                hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        }
+        setContentView(R.layout.activity_main_splash)
+        supportActionBar?.hide()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ),
+                    10
+                )
+            } else {
+                startMainActivity()
+            }
+        } else {
+            startMainActivity()
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineContext.cancelChildren()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (grantResults.isEmpty() || grantResults.contains(PackageManager.PERMISSION_DENIED)) {
+                Log.d("SPLASH", "권한 ${grantResults[0]}")
+                Toast.makeText(this, "위치 권한이 없으면 앱을 실행 할 수 없습니다.", Toast.LENGTH_LONG).show()
+
+                Handler(Looper.getMainLooper()).postDelayed({
+                    finishAffinity()
+                    exitProcess(2)
+                }, 3000)
+
+            } else {
+                startMainActivity()
+            }
+        }
+    }
+
+    private fun startMainActivity() {
+        Log.d("MainSplashActivity", "startMainActivity")
+
+        launch {
+            Log.d("MainSplashActivity", "Coroutines")
+            delay(3000)
+
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
