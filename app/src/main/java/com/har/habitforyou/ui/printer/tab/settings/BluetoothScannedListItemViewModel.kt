c57d8059ab818ab7a9705658ce7fd4c4f2d67dd8
SnowWaters <harharr@gmail.com>
2020-11-11 00:16:22 +0900

    [Bluetooth] Update List Item

--- app/src/main/java/com/har/habitforyou/ui/printer/tab/settings/BluetoothScannedListItemViewModel.kt
+++ app/src/main/java/com/har/habitforyou/ui/printer/tab/settings/BluetoothScannedListItemViewModel.kt
@@ -1,5 +1,6 @@
 package com.har.habitforyou.ui.printer.tab.settings
 
+import android.bluetooth.BluetoothClass
 import android.bluetooth.BluetoothDevice
 import androidx.lifecycle.ViewModel
 
@@ -10,4 +11,20 @@ class BluetoothScannedListItemViewModel(private val bluetoothDevice: BluetoothDe
 
     val address: String
         get() = bluetoothDevice.address ?: "--:--:--:--:--:--"
+
+
+    // 프린터 -> Imaging
+    // TV -> Video / Audio
+    // 폰/태블릿 -> Phone
+    val type:String
+        get() {
+            return when(bluetoothDevice.bluetoothClass.majorDeviceClass) {
+                BluetoothClass.Device.Major.IMAGING -> "Printer"
+                BluetoothClass.Device.Major.AUDIO_VIDEO -> "Audio/Video"
+                BluetoothClass.Device.Major.PERIPHERAL -> "Peripheral"
+                BluetoothClass.Device.Major.PHONE -> "Phone/Tablet"
+                BluetoothClass.Device.Major.COMPUTER -> "Computer"
+                else -> "Devices"
+            }
+        }
 }
