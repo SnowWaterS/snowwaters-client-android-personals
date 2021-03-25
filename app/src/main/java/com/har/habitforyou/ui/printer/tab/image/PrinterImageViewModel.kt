0689dc7899d18f648f42a59d3644d5896df60716
SnowWaters <harharr@gmail.com>
2021-03-25 19:36:00 +0900

    [PhotoPrinter] add base wireframe

--- app/src/main/java/com/har/habitforyou/ui/printer/tab/image/PrinterImageViewModel.kt
+++ app/src/main/java/com/har/habitforyou/ui/printer/tab/image/PrinterImageViewModel.kt
@@ -1,7 +1,23 @@
 package com.har.habitforyou.ui.printer.tab.image
 
+import android.view.View
 import androidx.lifecycle.ViewModel
 
 class PrinterImageViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    fun onTakePhotoBtnClicked(view: View) {
+    }
+
+    fun onLoadPhotoBtnClicked(view: View) {
+
+    }
+
+    fun onRemovePhotoBtnClicked(view: View) {
+
+    }
+
+    fun onPrintPhotoBtnClicked(view: View) {
+
+    }
+
 }
