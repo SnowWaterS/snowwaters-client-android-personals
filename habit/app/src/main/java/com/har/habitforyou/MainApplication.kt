1bdd6423e07ecc069fb69ca7b158e5debd005cbf
SnowWaterS <harharr@gmail.com>
2021-06-07 08:56:10 +0900

    [habbit] set up hilt

--- habit/app/src/main/java/com/har/habitforyou/MainApplication.kt
+++ habit/app/src/main/java/com/har/habitforyou/MainApplication.kt
@@ -2,7 +2,9 @@ package com.har.habitforyou
 
 import android.app.Application
 import com.har.habitforyou.util.ContextUtil
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MainApplication: Application() {
 
     override fun onCreate() {
