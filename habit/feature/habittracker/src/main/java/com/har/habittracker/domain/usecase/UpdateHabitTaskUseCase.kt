6d25e08a90f3c4bcf7c497850a59c035fad2f9d1
SnowWaterS <harharr@gmail.com>
2021-06-24 22:30:20 +0900

    [habit] Habit Tracker - add usecase

---
+++ habit/feature/habittracker/src/main/java/com/har/habittracker/domain/usecase/UpdateHabitTaskUseCase.kt
@@ -0,0 +1,10 @@
+package com.har.habittracker.domain.usecase
+
+import com.har.habittracker.domain.model.HabitTask
+import com.har.habittracker.domain.repository.IHabitTaskRepository
+
+class UpdateHabitTaskUseCase(private val repository: IHabitTaskRepository) {
+    suspend fun execute(habitTask: HabitTask) {
+        repository.updateHabitTask(habitTask)
+    }
+}
