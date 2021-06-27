3ada5458259a626eb6c9f08008c23fa58241039c
SnowWaterS <harharr@gmail.com>
2021-06-27 22:53:26 +0900

    [habit] Habit Tracker - add habitTaskLogs

---
+++ habit/feature/habittracker/src/main/java/com/har/habittracker/domain/model/HabitTaskLog.kt
@@ -0,0 +1,23 @@
+package com.har.habittracker.domain.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import java.util.*
+
+@Entity(tableName = HabitTaskLog.TABLE_NAME,
+        indices = [Index(value = ["habit_task_id"])],
+        foreignKeys = [ForeignKey(
+                entity = HabitTask::class,
+                childColumns = ["habit_task_id"],
+                parentColumns = ["id"],
+                onDelete = ForeignKey.CASCADE)])
+data class HabitTaskLog(
+        @ColumnInfo(name = "habit_task_id") val habitTaskId: Long,
+        @ColumnInfo(name = "check_date") val checkDate: Date
+) {
+    companion object {
+        const val TABLE_NAME = "habit_task_logs"
+    }
+}
--- habit/feature/habittracker/src/main/java/com/har/habittracker/domain/repository/IHabitTaskLogRepository.kt
+++ habit/feature/habittracker/src/main/java/com/har/habittracker/domain/repository/IHabitTaskLogRepository.kt
@@ -1,4 +1,14 @@
 package com.har.habittracker.domain.repository
 
+import androidx.lifecycle.LiveData
+import com.har.habittracker.domain.model.HabitTaskLog
+
 interface IHabitTaskLogRepository {
+
+    fun getHabitTaskLogById(habitTaskId: Long): LiveData<List<HabitTaskLog>>
+
+    fun checkHabitTaskLog(habitTaskId: Long)
+
+    fun cancelHabitTaskLog(habitTaskId: Long)
+
 }
