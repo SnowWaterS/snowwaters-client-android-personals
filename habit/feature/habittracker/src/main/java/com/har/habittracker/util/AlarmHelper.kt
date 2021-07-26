f3b14b613b08cc7ed98c2782cd2ab2ebd7e3b037
SnowWaterS <harharr@gmail.com>
2021-07-26 22:23:14 +0900

    [habit] Habit Tracker - set notification

--- habit/feature/habittracker/src/main/java/com/har/habittracker/util/AlarmHelper.kt
+++ habit/feature/habittracker/src/main/java/com/har/habittracker/util/AlarmHelper.kt
@@ -8,6 +8,7 @@ import android.content.Intent
 import android.graphics.Color
 import android.provider.Settings
 import androidx.core.app.NotificationCompat
+import com.har.habittracker.R
 import com.har.habittracker.presentation.HabitTrackerActivity
 
 class AlarmHelper {
@@ -16,24 +17,29 @@ class AlarmHelper {
         val intent = Intent(context, HabitTrackerActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
 
-        val resultPendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val resultPendingIntent =
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
         val builder = NotificationCompat.Builder(context, "10001")
         builder.setContentTitle("Test")
-                .setContentText("AlarmTest")
-                .setAutoCancel(false)
-                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
-                .setContentIntent(resultPendingIntent)
+            .setContentText("AlarmTest")
+            .setSmallIcon(R.drawable.ic_logo)
+            .setAutoCancel(false)
+            .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+            .setContentIntent(resultPendingIntent)
 
-        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             val importance = NotificationManager.IMPORTANCE_HIGH
-            val notificationChannel = NotificationChannel("10001", "NOTIFICATION_CHANNEL_NAME", importance)
+            val notificationChannel =
+                NotificationChannel("10001", "NOTIFICATION_CHANNEL_NAME", importance)
             notificationChannel.enableLights(true)
             notificationChannel.lightColor = Color.RED
             notificationChannel.enableVibration(true)
-            notificationChannel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+            notificationChannel.vibrationPattern =
+                longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
 
             notificationManager.createNotificationChannel(notificationChannel)
         }
